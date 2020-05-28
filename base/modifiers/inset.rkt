d5bd74812cda79989ba865465663ce3bd0adcfa9
Simon 'Sze' L. Schlee <schlee.simon@gmail.com>
2020-05-28 17:22:17 +0200

    inset with more specific and combinable options

--- base/modifiers/inset.rkt
+++ base/modifiers/inset.rkt
@@ -3,20 +3,37 @@
 (provide inset)
 
 (require racket/contract
+         racket/match
          "../types.rkt"
          "../mode.rkt")
 
-(define/contract (inset offset content)
-  (-> pr2? sizeable/c sizeable/c)
-  (define twice (pr2* offset pr2-two))
+(define/contract (inset content
+                        #:size   [size   pr2-zero]
+                        #:left   [left   pr-0]
+                        #:right  [right  pr-0]
+                        #:top    [top    pr-0]
+                        #:bottom [bottom pr-0])
+  (->* (sizeable/c)
+       (#:size   pr2?
+        #:left   pr?
+        #:right  pr?
+        #:top    pr?
+        #:bottom pr?)
+       sizeable/c)
+  (match-define (vec2 h v) size)
+  (inset/impl (pr+ left h) (pr+ right h) (pr+ top v) (pr+ bottom v) content))
+
+(define (inset/impl left right top bottom content)
+  (define lt (pr2 left  top))
+  (define rb (pr2 right bottom))
   (λ (size)
-    (define inner-size (pr2->pos (pr2- (pos->pr2 size) twice)
+    (define inner-size (pr2->pos (pr2- (pr2- (pos->pr2 size) lt) rb)
                                  size))
     (define piece (apply-wrapper content inner-size))
     (container (λ (self)
-                 (pr2+ twice (ui-bounds piece)))
+                 (pr2+ (pr2+ lt (ui-bounds piece)) rb))
                (λ (self pos callback)
-                 (callback piece (pos+ pos (pr2->pos offset size)))))))
+                 (callback piece (pos+ pos (pr2->pos lt size)))))))
 
 (module+ test
   (require rackunit/chk
