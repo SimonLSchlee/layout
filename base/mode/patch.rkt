9513c610ac116207a99c7f30d1d9fb42bcd62477
Simon 'Sze' L. Schlee <schlee.simon@gmail.com>
2020-05-29 19:25:56 +0200

    apply patch implementation

--- base/mode/patch.rkt
+++ base/mode/patch.rkt
@@ -1,18 +1,24 @@
 #lang racket/base
 
 (require racket/contract
+         racket/match
+         racket/list
          "../data.rkt"
          "../private/parameter.rkt"
          "reference.rkt")
 
 (define patch-accumulator/c (-> reference? ref/kind/c any/c))
 
-(provide (contract-out
-          [patched patch-accumulator/c])
-         current-patch)
+(provide current-patch
+         changes/c
+         (contract-out
+          [struct change ([ref   reference?]
+                          [value ref/kind/c])]
+          [patched patch-accumulator/c]
+          [apply-patch (-> data/c changes/c data/c)]))
 
-(struct change        (path value) #:prefab)
-(define changes/c     (listof change?))
+(struct change    (ref value) #:transparent)
+(define changes/c (listof change?))
 (define-guarded-parameter current-patch changes/c null)
 
 
@@ -22,4 +28,32 @@
                        (current-patch))))
 
 (define (apply-patch data patch)
-  (raise "impl apply-patch"))
+  (define (apply-change data ref value)
+    (define (update path current new)
+      (define f (first path))
+      (define r (rest path))
+      (if (empty? r)
+          (match current
+            [(? hash?) (hash-set current f new)]
+            [(? list?) (list-set current f new)])
+          (match f
+            [(? symbol? s)
+             (hash-update current s
+                          (λ (old)
+                            (update r old new))
+                          hash)]
+            [(? path-index? i)
+             (define len (length current))
+             (define old
+               (if (i . < . len)
+                   (list-ref current i)
+                   (raise "apply-patch auto generating new list elements is not implemented")))
+             (list-set current i (update r old new))])))
+
+    (define path (ref->path ref))
+    (update path data value))
+
+  (for/fold ([data data])
+            ([c (in-list (reverse patch))])
+    (match-define (change ref value) c)
+    (apply-change data ref value)))
